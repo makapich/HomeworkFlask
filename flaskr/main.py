7856c0b97ceeccf7af8d79ba9901ef164a596a06
makapich <makarovtima05@gmail.com>
2023-02-25 07:54:16 +0100

    Home work 2(Flask SQL) added

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,17 +1,19 @@
-from flask import Flask, Response
+from flask import Flask, Response, Blueprint
 from faker import Faker
 import pandas as pd
 import requests
 import flask
 
+main = Blueprint('main', __name__, url_prefix='/main')
 
-app = Flask(__name__)
 
-@app.route("/")
+#app = Flask(__name__)
+
+@main.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route('/requirements/')
+@main.route('/requirements/')
 def show_requirements():
     with open('requirements.txt', encoding="utf-8") as rt:
         req = rt.read()
@@ -21,7 +23,7 @@ def show_requirements():
 
 
 
-@app.route('/generate-users/')
+@main.route('/generate-users/')
 def generate_users():
     faker = Faker()
     count = int(flask.request.args.get('count', default=100))
@@ -33,20 +35,20 @@ def generate_users():
 
 
 
-@app.route('/mean/')
+@main.route('/mean/')
 def mean():
-    dataset = pd.read_csv('hw.csv')
+    dataset = pd.read_csv('../hw.csv')
     average = dataset.mean()
 
     return f"Средний рост {round(average.values[1]*2.54)} сантиметра, средний вес {round(average.values[2]/2.2)} килограмм"
 
 
-@app.route('/space/')
+@main.route('/space/')
 def get_astronauts():
     r = requests.get('http://api.open-notify.org/astros.json')
     astronauts_count = r.json()['number']
     response = flask.jsonify(count=astronauts_count)
     return response
 
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+ #   app.run()
